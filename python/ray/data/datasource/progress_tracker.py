21cdda76de59939908038ba4e7e5271020818ae5
Tony Francis <tony@dream3d.com>
2024-03-20 12:27:21 -0400

    concurrency groups
    Signed-off-by: Tony Francis <tony@dream3d.com>

--- python/ray/data/datasource/progress_tracker.py
+++ python/ray/data/datasource/progress_tracker.py
@@ -1,3 +1,4 @@
+import asyncio
 import atexit
 import json
 import logging
@@ -68,7 +69,7 @@ class Progress:
         )
 
 
-@ray.remote
+@ray.remote(concurrency_groups={"write": 1000, "sync": 1})
 class ProgressTracker:
     def __init__(self, save_path: str, save_interval: int = 10_000):
         self.save_path = save_path
@@ -79,12 +80,20 @@ class ProgressTracker:
         self.completed_queue = Queue(maxsize=save_interval)
 
         self.lock = False
-
         atexit.register(self.write)
         self.init_signal_handlers()
+    
+    async def acquire_lock(self):
+        # Wait until you can safely write to the pending queue
+        sleep = 1
+        while self.lock:
+            logger.debug(f"Waiting to acquire lock for pending queue. Sleeping for {sleep} seconds.")
+            await asyncio.sleep(sleep)
+            sleep *= 2
+        return True
 
     def sigkill_handler(self, signum, frame):
-        self.write()
+        asyncio.run(self.write())
 
         if self.original_handlers.get(signum):
             self.original_handlers[signum](signum, frame)
@@ -98,15 +107,22 @@ class ProgressTracker:
     def get_initial_progress(self) -> Progress:
         return self.initial_progress
 
-    def get_pending_queue(self) -> Queue:
+    @ray.method(concurrency_group="write")
+    async def get_pending_queue(self) -> Queue:
         # Call put_nowait_batch on this queue
+        await self.acquire_lock()
         return self.pending_queue
 
-    def get_completed_queue(self) -> Queue:
+    @ray.method(concurrency_group="write")
+    async def get_completed_queue(self) -> Queue:
         # Call put on this queue, and write when it is full
+        await self.acquire_lock()
         return self.completed_queue
 
-    def sync(self):
+    async def _sync(self):
+        logger.debug("Syncing progress tracker")
+
+        self.lock = True
         # get everything from the completed queue
         completed_keys: set[Key] = set()
         while self.completed_queue.qsize() > 0:
@@ -133,14 +149,17 @@ class ProgressTracker:
                     self.progress.completed[path].add(key)
                     self.progress.pending[path].remove(key)
                     break
+        
+        self.lock = False
 
-    def write(self):
+    @ray.method(concurrency_group="sync")
+    async def write(self):
         try:
             import fsspec
         except ImportError:
             raise ImportError("Please install fsspec")
 
-        self.sync()
+        await self._sync()
 
         logger.debug(f"Writing progress tracker to {self.save_path}")
         with fsspec.open(self.save_path, "wb", compression="gzip") as f:
